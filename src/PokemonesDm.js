13822e96c567bf1eea953f13960afd6e31fd2847
Miguel Tafur <migueltafurr@gmail.com>
2024-10-27 21:18:49 -0500

    fetch consumo apis Pokemon

--- src/PokemonesDm.js
+++ src/PokemonesDm.js
@@ -1,46 +1,106 @@
-import { LitElement, html } from 'lit-element';
-import { getComponentSharedStyles } from '@bbva-web-components/bbva-core-lit-helpers';
-import styles from './pokemones-dm.css.js';
+import { LitElement } from 'lit-element';
 
-/**
- * ![LitElement component](https://img.shields.io/badge/litElement-component-blue.svg)
- *
- * This component ...
- *
- * Example:
- *
- * ```html
- *   <pokemones-dm></pokemones-dm>
- * ```
- */
+
+
 export class PokemonesDm extends LitElement {
-  static get properties() {
-    return {
-      /**
-       * Description for property
-       */
-      name: {
-        type: String,
-      },
-    };
+
+
+
+  fetchPokemonList(url = 'https://pokeapi.co/api/v2/pokemon?limit=100') {
+    return fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const promises = data.results.map((pokemon) => this.fetchPokemonDetails(pokemon.name));
+        return Promise.all(promises).then((pokemons) => {
+          const basePokemons = pokemons.filter((pokemon) => pokemon.isBasePokemon);
+
+          this.arrayPokemon = [...this.arrayPokemon, ...basePokemons];
+          this.nextUrl = data.next;
+
+          this.dispatchEvent(new CustomEvent('pokemonesFetched', {
+            detail: {
+              pokemons: this.arrayPokemon,
+              nextUrl: this.nextUrl,
+            },
+          }));
+        });
+      })
+      .catch((error) => {
+        console.error('Error al consumir la lista:', error);
+        this.dispatchEvent(new CustomEvent('errorFetch', { detail: { error } }));
+      });
   }
 
-  constructor() {
-    super();
-    this.name = 'Cells';
+
+  fetchPokemonDetails(pokemonName) {
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const { name, sprites, species, types } = data;
+        const typesNames = types.map((typeInfo) => typeInfo.type.name).join(', ');
+
+        return fetch(species.url)
+          .then((response) => response.json())
+          .then((speciesData) => {
+            const isBasePokemon = !speciesData.evolves_from_species;
+
+            return {
+              name,
+              image: sprites.other.dream_world.front_default,
+              speciesUrl: species.url,
+              types: typesNames,
+              isBasePokemon,
+            };
+          });
+      })
+      .catch((error) => {
+        console.error('Error al consumir los detalles:', error);
+      });
   }
 
-  static get styles() {
-    return [
-      styles,
-      getComponentSharedStyles('pokemones-dm-shared-styles'),
-    ];
-  }
+  fetchEvolutionChain(speciesUrl) {
+    fetch(speciesUrl)
+      .then((response) => response.json())
+      .then((speciesData) => fetch(speciesData.evolution_chain.url))
+      .then((response) => response.json())
+      .then((evolutionData) => {
+        const fetchEvolutionImages = async (species) => {
+          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${species.name}`);
+          const data = await response.json();
+          const typesNames = data.types.map((typeInfo) => typeInfo.type.name).join(', ');
+          return {
+            name: species.name,
+            types: typesNames,
+            image: data.sprites.other.dream_world.front_default || data.sprites.front_default,
+          };
+        };
 
-  render() {
-    return html`
-      <p>Welcome to ${this.name}</p>
-      <slot></slot>
-    `;
+        const processEvolutionChain = async (chain) => {
+          const evolutions = [];
+          let currentEvolution = chain;
+
+          while (currentEvolution) {
+            const evolutionDetails = await fetchEvolutionImages(currentEvolution.species);
+            evolutions.push(evolutionDetails);
+            currentEvolution = currentEvolution.evolves_to[0];
+          }
+
+          return evolutions;
+        };
+
+        processEvolutionChain(evolutionData.chain)
+          .then((evolutions) => {
+            this.dispatchEvent(new CustomEvent('evolucionesFetched', { detail: { evolutions } }));
+          })
+          .catch((error) => {
+            console.error('Error al procesar la cadena de evolución:', error);
+            this.dispatchEvent(new CustomEvent('errorFetch', { detail: { error } }));
+          });
+      })
+      .catch((error) => {
+        console.error('Error al consumir la evolucion', error);
+        this.dispatchEvent(new CustomEvent('errorFetch', { detail: { error } }));
+      });
   }
 }
+
